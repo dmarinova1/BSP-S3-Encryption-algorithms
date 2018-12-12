19edd31a1fdd2b781a87034e3dbe8962101c4d17
dmarinova1 <38781094+dmarinova1@users.noreply.github.com>
2018-12-12 18:45:57 +0100

    Add files via upload

--- client.py
+++ client.py
@@ -1,27 +1,161 @@
 import socket
 import sys
+import base64
+import hashlib
+import os
+from Cryptodome.Cipher import AES
+from Cryptodome.Hash import SHA256
+from Cryptodome import Random
+from Cryptodome.Cipher import DES
+from Cryptodome.Util.Padding import unpad
+from timeit import default_timer as time
 
-
-
-ip = str(socket.gethostbyname(socket.gethostname()))
-print("The IP Address is: " + ip)
+s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = str(socket.gethostbyname(socket.gethostname()))
 port = 1234
+s.connect((host,port))
+print("Connected to chat server")
 
-#create a socket as communication endpoint using the TCP protocol
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print("Socket created.")
-    #connect the client's socket with the server's socket as the given IP and port number
-s.connect((ip, port))
-print("Socket connected to " + ip)
 
-message = input("Type message: ")
-try:
-    s.sendall(message.encode()) #send some data
-except socket.error:
-    print("Not sent.")
-    sys.exit()
-print("Message sent.")
-data = s.recv(4096) #get server's reply
-print(data.decode())
-s.close() #we close socket that allows connections to exist
+
+def encryptCaeser(message):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    key = 3
+    newMessage = ""
+    
+    for char in message:
+        if char in alphabet:
+            position = alphabet.find(char)
+            newPosition = (position + key) % 26
+            newChar = alphabet[newPosition]
+            newMessage += newChar
+        else:
+            newMessage += char
+
+    return newMessage
+
+
+def encrypt_rot13():
+    message = input("Enter you message to be encrypted: ").upper()
+    
+    key = 13
+    encrypt_text = ""
+    for i in range(len(message)):
+        temp = ord(message[i]) + key
+        if ord(message[i]) == 32:
+            encrypt_text += " "
+        elif temp > 90: #if temp > uppercase Z substract 26 and go back to A; we note that in ASCII characters lowercase letters from a to z go from 97 to 122
+            temp -= 26
+            encrypt_text += chr(temp)
+        else:
+            encrypt_text += chr(temp)
+    return encrypt_text
+    #print("Encrypted message using ROT13:", encrypt_text)
+
+
+def make_key(aespassword):
+
+    key = hashlib.sha256(aespassword.encode("utf-8")).digest()
+    return key
+
+
+def aesencrypt(message, key):
+    #   before encrypting a plaintext of X bytes, we append to the back as many bytes we need to to reach the next 16 byte boundary.
+    pad = AES.block_size - len(message) % AES.block_size
+    try:
+        message = message.decode()
+    except:
+        pass
+    message = message + pad * chr(pad)
+
+    iv = os.urandom(16)
+    fout_iv = open("iv.dat", "wb")
+    fout_iv.write(iv)
+    fout_iv.close()
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    ciphertext = cipher.encrypt(message.encode("utf-8"))
+    print(ciphertext)
+    ciphertext = base64.b64encode(ciphertext)
+
+    return ciphertext
+
+
+def despad(text):
+    while len(text) % 8 != 0:
+        text += ' '
+    return text
+
+while True:
+    key = "mysecret"
+    des = DES.new(key.encode("utf-8"), DES.MODE_ECB)
+
+    choice1 = int(input("1.Encryption with Caeser cipher\n2.Encryption with ROT13\n3.Encryption with AES\n4.Encryption with DES\n5.Go back\nChoose 1,2,3,4 or 5: "))
+    if choice1 == 1:
+        message = input("What is the message you would like to have encrypted?: ")
+        print("---Encrypting with Caeser cipher---")
+        start = time()
+        encrypted_text = encryptCaeser(message)
+        print(encryptCaeser(message))
+        end = time()
+        s.send(encrypted_text.encode())
+        print("Message sent.")
+        print("")
+        print("Encryption with Caeser cipher took %f seconds." % (end - start))
+
+    elif choice1 == 2:
+        print("---Encrypting with ROT13---")
+        start = time()
+        encrypted_text = encrypt_rot13()
+        print(encrypted_text)
+        end = time()
+        s.send(encrypted_text.encode())
+        print("Message sent.")
+        print("")
+        print("Encryption with ROT13 took %f seconds." % (end - start))
+            
+    elif choice1 == 3:
+        aespassword = input("Enter AES encryption password: ")
+        aesinput1 = input("Enter your message to be encrypted: ")
+        key = make_key(aespassword)
+        start = time()
+        ciphertext = aesencrypt(aesinput1, key)
+        print(b"The ciphertext is: " + ciphertext)
+        end = time()
+        s.send(ciphertext)
+        print("Message sent.")
+        print("")
+        print("Encryption with AES took %f seconds." % (end - start))
+
+        # Write the cipher and the key in separate files
+        fout_cipher = open("cipher.dat", "wb")
+        fout_cipher.write(ciphertext)
+        fout_cipher.close()
+
+        fout_key = open("key.dat", "wb")
+        fout_key.write(key)
+        fout_key.close()
+
+    elif choice1 == 4:
+        text1 = input("Enter your message: ")
+        padded_text = despad(text1)
+        start = time()
+        encrypted_text = des.encrypt(padded_text.encode("utf-8"))
+        encrypted_text = base64.b64encode(encrypted_text)
+        print(encrypted_text)
+        end = time()
+        s.send(encrypted_text)
+        print("Message sent.")
+        print("")
+        print("It took %f seconds to encrypt the message using DES." % (end - start))
+
+    elif choice1 == 5:
+        break
+
+    else:
+        print("Invalid choice")
+        
+        incoming_message = s.recv(4096)
+        incoming_message = incoming_message.decode()
+        print(" Server : ", incoming_message.lower())
+        print("")
 
--- server.py
+++ server.py
@@ -1,41 +1,151 @@
 import socket
-import threading
+import sys
+import base64
+import hashlib
+import os
+from Cryptodome.Cipher import AES
+from Cryptodome.Hash import SHA256
+from Cryptodome import Random
+from Cryptodome.Cipher import DES
+from Cryptodome.Util.Padding import unpad
+from timeit import default_timer as time
 
-# creates a variable ip which holds the ip of the machine it is running on
-ip = str(socket.gethostbyname(socket.gethostname()))
-port = 1234
+def decryptCaeser(message):
+    message = message
+    key = 3
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    newMessage = ""
+    for char in message:
+        if char in alphabet:
+            position = alphabet.find(char)
+            newPosition = (position - key) % 26
+            newChar = alphabet[newPosition]
+            newMessage += newChar
+        
+        else:
+            newMessage += char
+    return newMessage
 
-class EchoThread (threading.Thread):
-    def __init__(self, conn, addr):
-        threading.Thread.__init__(self)
-        self.conn = conn
-        self.addr = addr
-
-    def run(self):
-        with conn:
-            print("connected by", addr, "in thread",threading.current_thread())
-            #keep echoing what the client sent, until the connection is closed then the EchoThread terminates
-            while True:
-                data = conn.recv(4096)
-                if not data: # if there is no data, the connection is broken
-                    break #break out of loop
-                conn.sendall(b'you said: ' + data) #send back to client the reply
-            print("connection closed with ", addr) # if loop is broken, connection is broken, thus, connection between client and server is closed
-
-#create a socket as communication endpoint using the TCP protocol
-s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print("Socket created.")
-    # bind the socket to the given IP (localhost) and port number
-    #listen for incoming connections (limited to 10)
-s.bind((ip, port))
-print("Socket has been bounded.")
-s.listen(10)
-print("The server is ready...")
-print("IP address of the server:%s" % ip)
+def decrypt_rot13(msg):
+    message = msg.upper() # work with uppercase letters
     
-#keep accepting many incoming connections in an infinite loop; create and launch a separate dedicated EchoThread to handle the communication with some client
+    key = 13
+    decrypt_text = ""
+    for i in range(len(message)):
+        temp = ord(message[i]) - key
+        if ord(message[i]) == 32: #check for space i.e. 32 is empty space in ASCII
+            decrypt_text += " "
+        elif temp < 65: #if temp < than uppercase A add 26 and move to Z
+            temp += 26
+            decrypt_text += chr(temp)
+        else:
+            decrypt_text += chr(temp)
+
+    return decrypt_text
+
+
+def aesdecrypt(ciphertext, key):
+
+    enc_msg = base64.b64decode(ciphertext)
+    fin_iv = open("iv.dat", "rb")
+    iv = fin_iv.read()
+    fin_iv.close()
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    msg = unpad(cipher.decrypt(enc_msg), 16)
+    return msg.decode("utf-8")
+
+
+s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = str(socket.gethostbyname(socket.gethostname()))
+print(" server will start on host : ", host)
+port = 1234
+s.bind((host,port))
+print("")
+print("Host and port bounded successfully.")
+print("")
+print("Server is ready and waiting for incoming connections...")
+print("")
+s.listen(10)
+conn, addr = s.accept()
+print(addr, " has connected to the server and is now online.")
+print("")
 while True:
-    (conn, addr) = s.accept()
-    print('%s connected to the server' % str(addr))
-    echoThread = EchoThread(conn, addr)
-    echoThread.start()
+    key = "mysecret"
+    des = DES.new(key.encode("utf-8"), DES.MODE_ECB)
+    incoming_message = conn.recv(4096)
+    incoming_message = incoming_message.decode()
+    print(" Client : ", incoming_message)
+    print("")
+
+    choice2 = int(input("1.Decryption with Caeser cipher\n2.Decryption with ROT13\n3.Decryption with AES\n4.Decryption with DES\n5.Go back\nChoose 1,2,3,4 or 5: "))
+        
+        
+    if choice2 == 1:
+        message = incoming_message
+        print("---Decrypting with Caeser cipher---")
+        start = time()
+        decrypted_text = decryptCaeser(message)
+        print(decryptCaeser(message))
+        end = time()
+        conn.send(decrypted_text.encode())
+        print("Message sent.")
+        print("")
+        print("Decryption with Caeser cipher took %f seconds." % (end - start))
+                
+    elif choice2 == 2:
+ 
+        print("---Decrypting with ROT13---")
+        start = time()
+        decrypted = decrypt_rot13(incoming_message)
+        print(decrypted.lower())
+        end = time()
+        conn.send(decrypted.encode())
+        print("Message sent.")
+        print("")
+        print("Decryption with ROT13 took %f seconds." % (end - start))
+
+    elif choice2 == 3:
+        if not os.path.isfile("cipher.dat"):
+            print("WARN: There is no ciphertext for decryption.")
+            exit(1)
+        if not os.path.isfile("key.dat"):
+            print("WARN: The encryption key is missing.")
+            exit(1)
+
+        fin_cipher = open("cipher.dat", "rb")
+        ciphertext = fin_cipher.read()
+        fin_cipher.close()
+
+        fin_key = open("key.dat", "rb")
+        key = fin_key.read()
+        fin_key.close()
+
+        start = time()
+        decrypted = aesdecrypt(ciphertext, key)
+        print("The clear text is: " + decrypted)
+        end = time()
+        conn.send(decrypted.encode())
+        print("Message sent.")
+        print("")
+        print("Decryption with AES took %f seconds." % (end - start))
+
+
+    elif choice2 == 4:
+
+        desinput = incoming_message
+        desinput = base64.b64decode(desinput)
+        start = time()
+        decrypted_text = des.decrypt(desinput)
+        print(b"The cleartext is: " + decrypted_text)
+        end = time()
+        conn.send(decrypted_text)
+        print("Message sent.")
+        print("")
+        print("It took %f seconds." % (end - start))
+    
+    elif choice2 == 5:
+        break
+    else:
+        print("Invalid choice")
+            
+            
